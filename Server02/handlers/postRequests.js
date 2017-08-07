ecdbce3ed67b3e99531350561725e3ef90c0bbc7
Svetoslav Genchev <svetoslav.genchev@yahoo.com>
2017-08-07 08:02:55 +0300

    add json

--- Server02/handlers/postRequests.js
+++ Server02/handlers/postRequests.js
@@ -4,14 +4,20 @@ let query = require('querystring')
 module.exports = (req, res) => {
   if (req.method === 'POST') {
     let body = ''
+    let arr = []
     req.on('data', (data) => {
       body += data
     })
     req.on('end', () => {
       let result = query.parse(body)
-      console.log(result)
+      arr.push(result)
+      fs.writeFile('./db.json', 'module.exports = ' + JSON.stringify(result, null, 4), (err) => {
+        console.log(err)
+      })
+
       res.writeHead(200)
       res.write(body)
+      console.log(arr)
       res.end()
     })
   } else {
