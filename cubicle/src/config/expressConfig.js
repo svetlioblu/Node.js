3fed921ad0de2b7025c90f2b92443774ef6ff3d3
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-09 07:57:52 +0300

    add cookiePArser add jsonwebtoken updated expressConfig

--- cubicle/src/config/expressConfig.js
+++ cubicle/src/config/expressConfig.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 function expressConfig(app) {
     //express config to get static files
     app.use(express.static('src/assets'))
     // express config middlewear to parse post requests as object
     app.use(express.urlencoded({ extended: false }))
+    // adding cookies feature 
+    app.use(cookieParser())
 }
 module.exports = expressConfig
