9eb38167aefcc1f476fdcd920fbd3710514d8218
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-14 20:31:59 +0300

    add userController and view

--- petstagram/src/routes.js
+++ petstagram/src/routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
+const userController = require('./controllers/userController')
+
 
 router.use(homeController)
+router.use('/users',userController)
+
 
 
 
