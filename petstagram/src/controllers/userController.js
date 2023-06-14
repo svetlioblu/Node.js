46380f7661864c1adfe77030de7c500775fcc4a9
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-14 20:43:36 +0300

    add register get endpoint and vew

--- petstagram/src/controllers/userController.js
+++ petstagram/src/controllers/userController.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
 
 router.get('/login', (req, res) => {
-    res.render('login')
+    res.render('users/login')
 })
+
+
+
+router.get('/register', (req, res) => {
+    res.render('users/register')
+})
+
+
 
 module.exports = router
--- petstagram/src/routes.js
+++ petstagram/src/routes.js
@@ -4,6 +4,7 @@ const homeController = require('./controllers/homeController')
 const userController = require('./controllers/userController')
 
 
+
 router.use(homeController)
 router.use('/users',userController)
 
