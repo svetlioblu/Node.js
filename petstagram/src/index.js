b016295b0dce1c4c302df591179e71e638ffdf9c
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-14 07:29:29 +0300

    add handlebars config

--- petstagram/src/index.js
+++ petstagram/src/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+
+const routes = require('./routes')
+
+//config express
 const app = express()
 app.use(express.static('assets'))
 app.use(express.urlencoded({ extended: false }))
-
-const routes = require('./routes')
+// config handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', 'src/views')
+//config routes 
 app.use(routes)
-
 
 
 app.listen(5000, () => { console.log('The server listening on port 5000...') })
