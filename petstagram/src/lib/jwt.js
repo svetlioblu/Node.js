2833118125b22956b79a856d05c208bb299d24c0
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-15 17:07:17 +0300

    updated jwt.js ser token as cookie

--- petstagram/src/lib/jwt.js
+++ petstagram/src/lib/jwt.js
@@ -2,7 +2,7 @@ const util = require('util')
 const jsonwebtoken = require('jsonwebtoken')
 
 const jwt = {
-    sing: util.promisify(jsonwebtoken.sign),
+    sign: util.promisify(jsonwebtoken.sign),
     verify: util.promisify(jsonwebtoken.verify)
 }
 
