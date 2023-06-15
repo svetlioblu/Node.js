b53d5e9007f87b6d52eac962aaa437dc507c8a8e
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-15 07:42:59 +0300

    validate if user exists in userService

--- petstagram/src/services/userService.js
+++ petstagram/src/services/userService.js
@@ -4,6 +4,11 @@ exports.login = (username, password) => {
     // const user = User.findOne({ username: username })
 }
 
-exports.register = (userRegisterData) => {
-    User.create(userRegisterData)
+exports.register = async (userRegisterData) => {
+    const user = await User.findOne({ username: userRegisterData.username })
+    if (user) {
+        throw new Error('Username already Exists !')
+    }
+
+    return User.create(userRegisterData)
 }
