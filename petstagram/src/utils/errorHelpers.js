5b220413d627aa9b120dbcdf36135db5cf17ac2e
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-16 08:04:42 +0300

    add errorHelpers error extractor

---
+++ petstagram/src/utils/errorHelpers.js
@@ -0,0 +1,8 @@
+exports.getErrorMessage = (err) => {
+    if (err instanceof MongooseError) {
+        return Object.values(err.errors)[0].message
+    } else {
+        return err.message
+    }
+
+}
