ff7a69f94674fec6dcb3a1873e30334a086e024c
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-17 15:49:53 +0300

    add try/catch for delete and details

--- petstagram/src/controllers/catalogController.js
+++ petstagram/src/controllers/catalogController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const catalogService = require('../services/catalogService')
-
+const { getErrorMessage } = require('../utils/errorHelpers')
 
 router.get('/catalog', async (req, res) => {
     const cards = await catalogService.getAll().lean()
@@ -11,15 +11,24 @@ router.get('/catalog', async (req, res) => {
 
 router.get('/catalog/:name/:cardId', async (req, res) => {
     const cardId = req.params.cardId
-    const oneCard = await catalogService.getOne(cardId).lean()
-    const isOwner = req.user?._id == oneCard.owner._id
-    res.render('photos/details', { oneCard, isOwner })
+    try {
+        const oneCard = await catalogService.getOne(cardId).lean()
+        const isOwner = req.user?._id == oneCard.owner._id
+        res.render('photos/details', { oneCard, isOwner })
+    } catch (err) {
+        res.render('photos/catalog', { error: getErrorMessage(err) })
+    }
+
 })
 
 router.get('/catalog/:name/:cardId/delete', async (req, res) => {
     const cardId = req.params.cardId
-    
-    await catalogService.delete(cardId)
-    res.redirect('/catalog')
+    try {
+        await catalogService.delete(cardId)
+        res.redirect('/catalog')
+    } catch (err) {
+        res.render('photos/details', { error: 'Unsuccessful deleteion' })
+    }
+
 })
 module.exports = router
