1dd8eb8293085be600b27b6d0b9aa8bf4bd7a9cc
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-17 11:59:25 +0300

    add petCard partial

--- petstagram/src/controllers/catalogController.js
+++ petstagram/src/controllers/catalogController.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
+const catalogservice = require('../services/catalogService')
 
 router.get('/catalog', (req, res) => {
+
     res.render('photos/catalog')
 })
 
