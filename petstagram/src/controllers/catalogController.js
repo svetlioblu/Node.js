2526da55c4bd4b4a7ad047a2256edc54ede46f7f
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-17 13:28:45 +0300

    add isOwner in page details view

--- petstagram/src/controllers/catalogController.js
+++ petstagram/src/controllers/catalogController.js
@@ -10,8 +10,8 @@ router.get('/catalog', async (req, res) => {
 router.get('/catalog/:name/:cardId', async (req, res) => {
     const cardId = req.params.cardId
     const oneCard = await catalogService.getOne(cardId).lean()
-
-    res.render('photos/details', { oneCard })
+    const isOwner = req.user._id == oneCard.owner._id
+    res.render('photos/details', { oneCard, isOwner })
 })
 
 module.exports = router
