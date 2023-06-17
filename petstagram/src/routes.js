d2b49e622648382cbe9c971e96e4a96503145eb6
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-17 10:26:32 +0300

    add photoConroller and get

--- petstagram/src/routes.js
+++ petstagram/src/routes.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
 const userController = require('./controllers/userController')
+const photoController = require('./controllers/photoController')
 
 
 
 router.use(homeController)
 router.use('/users', userController)
+router.use(photoController)
 router.get('*', (req, res) => {
     res.redirect('/404')
 })
