91004c80b4bb7cf859c4ef21f0e034e456ee59f3
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-19 20:50:32 +0300

    add homeController sorting

--- FriendlyWorld/src/controllers/homeController.js
+++ FriendlyWorld/src/controllers/homeController.js
@@ -1,14 +1,16 @@
 const router = require('express').Router()
-
-router.get('/', (req, res) => {
-
+const animalService = require('../services/animalService')
+router.get('/', async (req, res) => {
+    const allAnimals = await animalService.getAll().limit(3).sort({'_id': 1}).lean()
+    console.log(allAnimals)
+  
     res.render('home')
 })
 
 
 router.get('/404', (req, res) => {
     res.render('404')
-    
+
 })
 
 
