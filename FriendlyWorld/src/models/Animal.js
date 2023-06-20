e820bddfcdadc7787f6d81896dc8f86b8a1732f8
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-20 07:13:05 +0300

    add animals validation i nthe Model

--- FriendlyWorld/src/models/Animal.js
+++ FriendlyWorld/src/models/Animal.js
@@ -3,20 +3,29 @@ const mongoose = require('mongoose')
 const animalSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Name is required !']
+        required: [true, 'Name is required !'],
+        minLength: [2, 'The name has to be at least 2 chars !'],
     },
     years: {
         type: Number,
         required: [true, 'Years link is required !'],
+        min: [1, 'The year is not correct, starts from 1'],
+        max: [100, 'The year is not correct']
+
+
     },
     kind: {
         type: String,
-        required: [true, 'Kind is required !']
+        required: [true, 'Kind is required !'],
+        minLength: [3, 'The kind has to be at least 2 chars !']
+
  
     },
     image: {
         type: String,
-        required: [true, 'Image is required !']
+        required: [true, 'Image is required !'],
+        match: [/^https?:\/\//, 'The imge format not match !']
+
     },
     need: {
         type: String,
