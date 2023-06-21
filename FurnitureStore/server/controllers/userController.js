4818968712ac3808dfa43b79ad0047db65cd6def
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-21 06:42:14 +0300

    add JOSN and urlEcoded middlewears

--- FurnitureStore/server/controllers/userController.js
+++ FurnitureStore/server/controllers/userController.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 
-
+router.post('/register', (req, res) => {
+    const { email, passwrd } = req.body
+ 
+})
 
 module.exports = router
--- FurnitureStore/server/index.js
+++ FurnitureStore/server/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 
+// This parse query strings. can use both middlewears
+app.use(express.urlencoded({ extended: false }))
+//RESTful recieves JSON post data(AJAX), regardless MPA , where used the above middlewear
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
     res.send('Hello')
 })
