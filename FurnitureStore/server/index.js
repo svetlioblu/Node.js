31ae125feb3f76abcea9bf6365410f217194fe98
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-21 17:20:29 +0300

    add auttMiddlewear in index.js

--- FurnitureStore/server/index.js
+++ FurnitureStore/server/index.js
@@ -4,6 +4,8 @@ const routes = require('./routes')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const { auth } = require('./middleWears/authMiddlewear')
+
 mongoose.connect('mongodb://127.0.0.1:27017/furnitures')
     .then(() => { console.log('DB Connected') })
     .catch(err => console.log(err))
@@ -13,7 +15,7 @@ app.use(express.urlencoded({ extended: false }))
 //RESTful recieves JSON post data(AJAX), regardless MPA , where used the above middlewear
 app.use(express.json())
 //CORS settings with 3 options config.
-app.use(cors())
+app.use(cors()) 
 // app.use(function (req, res, next) {
 //     res.setHeader('Access-Control-Allow-Origin', '*')
 //     res.setHeader('Access-Control-Allow-Methods',
@@ -23,9 +25,10 @@ app.use(cors())
 //     next()
 // })
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+app.get('/', (req, res) => res.send('Hello from RESTful server'))
+
 
 app.use(routes)
+app.use(auth)
+
 app.listen(3030, () => { console.log('RESTful server is listening on port 3030...') })
