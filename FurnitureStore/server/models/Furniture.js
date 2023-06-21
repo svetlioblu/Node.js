b498bc290152fe821d5adbb778cda1d073bef519
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-21 06:25:37 +0300

    add Furniture model

--- FurnitureStore/server/models/Furniture.js
+++ FurnitureStore/server/models/Furniture.js
@@ -1,7 +1,33 @@
 const mongoose = require('mongoose')
 
 const furnitureSchema = new mongoose.Schema({
-
+    make: {
+        type: String,
+        required:true
+    },
+    model: {
+        type: String,
+        required:true
+    },
+    year: {
+        type: Number,
+        required:true
+    },
+    description: {
+        type: String,
+        required:true
+    },
+    price: {
+        type: Number
+    },
+    image: {
+        type: String,
+        required:true
+    },
+    material: {
+        type: String,
+        required:true
+    }
 })
 
 const Furniture = mongoose.model('Furniture', furnitureSchema)
