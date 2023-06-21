9b165936c60548c2094f70d7ac894645c2542e15
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-21 08:41:49 +0300

    add login userService add getAuthResult common function userService

--- FurnitureStore/server/services/userService.js
+++ FurnitureStore/server/services/userService.js
@@ -1,6 +1,40 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-exports.register = (userData) => {
-    return User.create(userData)
+exports.register = async (userData) => {
+    const user = await User.create(userData)
+
+    const result = getAuthResult(user)
+    return result
 }
 
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error('Invalid username or password!')
+    }
+    const isValid = await bcrypt.compare(password, user.password)
+
+    if (!isValid) {
+        throw new Error('Invalid username or Password!!')
+    }
+
+    const result = getAuthResult(user)
+    return result
+}
+
+function getAuthResult(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email
+    }
+    const token = jwt.sign(payload, 'SECRETsecret', { expiresIn: '2d' })
+
+    const result = {
+        _id: user._id,
+        email: user.email,
+        accessToken: token,
+    }
+    return result
+}
