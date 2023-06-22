7549c013a62b63121efba7246b02753d67ae9184
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-22 07:00:14 +0300

    add ownerId in Furniture model

--- FurnitureStore/server/models/Furniture.js
+++ FurnitureStore/server/models/Furniture.js
@@ -28,6 +28,11 @@ const furnitureSchema = new mongoose.Schema({
     material: {
         type: String,
         required: true
+    },
+    _ownerId:{
+        type:mongoose.Types.ObjectId,
+        ref:'User'
+
     }
 })
 
