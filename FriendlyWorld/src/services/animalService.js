a978fe8be94fd4529ba27f9b33bf47761ffe52c0
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-23 08:13:07 +0300

    add donate animalService

--- FriendlyWorld/src/services/animalService.js
+++ FriendlyWorld/src/services/animalService.js
@@ -16,3 +16,10 @@ exports.edit = (animalId, editData) => {
 exports.delete = (animalId) => {
     return Animal.findByIdAndDelete(animalId)
 }
+
+exports.donate = async (animalId, ownerId) => {
+    const currentDonatedAnimal = await Animal.findById(animalId)
+    currentDonatedAnimal.donations.push({ user:ownerId })
+    currentDonatedAnimal.save()
+ return
+}
